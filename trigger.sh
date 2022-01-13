52d811026b9df14b8ff48074507256722bdb9313
Diego Costa <diego.costa7711@gmail.com>
2022-01-13 12:45:03 -0400

    test branch

--- trigger.sh
+++ trigger.sh
@@ -1,6 +1,9 @@
 folder=$(git diff --name-only HEAD~ HEAD | cut -d'/' -f 1)
 if [ "$folder" = "dummy" ]; then
     echo $folder
+    
 else
     echo "another one"
+    cd "add/"
+    cat "Readme.md"
 fi
